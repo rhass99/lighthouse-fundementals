fd3bf57c789574a051d72053d50f4c338602fde2
Rami H <rhass99@gmail.com>
2019-05-01 04:34:18 +0000

    Adds looping exercises

--- go-outside.js
+++ go-outside.js
@@ -10,3 +10,13 @@ if (cold) {
 }
 
 console.log("Now you're ready to go outside!");
+
+var vacationDays = 2 + 3;
+
+if (vacationDays <= 5) {
+  console.log("Not enough for a long vacation.");
+} else if (vacationDays === 5) {
+  console.log("We can take a whole week off, not bad.");
+} else {
+  console.log("We're gonna have a great time!");
+}
